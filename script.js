974a72d62bd5e18fd30a27e797a5e7d6769a2c82
Humberto Smith da Silva <93011482+humbsmith84@users.noreply.github.com>
2024-10-11 19:04:00 -0300

    a

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const weight = getElementById('weight').value;
-    const height = getElementById('height').value;
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
 
     const bmi = (weight / (height * height)).toFixed(2);
 
