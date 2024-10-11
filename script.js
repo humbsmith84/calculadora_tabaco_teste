c9fa84ede65afc8c261e03581580708d1828cd0a
Humberto Smith da Silva <93011482+humbsmith84@users.noreply.github.com>
2024-10-11 20:44:24 -0300

    a

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', function(event) {
     const cigarette = document.getElementById('cigarette').value;
     const years = document.getElementById('years').value;
 
-    const bmi = (cigarette + cigarette).toFixed(2);
+    const bmi = (years * (cigarrete / 20)).toFixed(2);
 
     const value = document.getElementById('value');
     let description = 'Teste calculadora tabagica';
